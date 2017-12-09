1803bf81329fd297c249c3261c9b6afbcc51d633
Stephen Johnson <sjohnson111@horizon.csueastbay.edu>
2017-12-08 20:40:10 -0800

    Player object moves and is network aware
    Movement is just forward and back with w and d, with rotation with a and s
    right now until I can get the camera attached to the face of the player.
    
    Networking is working. Building the project and starting a host, then
    starting the game in Unity and starting a client creates two players, which
    move on both instances (using localhost).

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //public
+        public float speed = 6f;
+
+    //private
+        private Vector3 movement;
+        private Rigidbody playerRB;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        float hMove = Input.GetAxisRaw("Horizontal");
+        float vMove = Input.GetAxisRaw("Vertical");
+	}
+
+    void Move(float hMove, float vMove)
+    {
+        movement.Set(hMove, 0f, vMove);
+
+        movement = movement.normalized * speed * Time.deltaTime;        //normalized prevents moving faster diagonally
+
+        playerRB.MovePosition(transform.position + movement);
+    }
+}
